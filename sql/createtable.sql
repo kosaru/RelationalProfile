eaa0b45cd87b9d3eb41171177b7adf2d8c8585b4
Masaru Kobashigawa <masaru.kobashigawa@gmail.com>
2013-07-03 01:18:06 +0900

    Update createtable.sql

--- sql/createtable.sql
+++ sql/createtable.sql
@@ -1,13 +1,22 @@
 CREATE TABLE "profile" (
 "id"  SERIAL ,
-"name" TEXT NOT NULL ,
-"ruby" TEXT ,
-"zip" TEXT ,
-"address" TEXT ,
-"tel" TEXT ,
-"fax" TEXT ,
+"name" TEXT NOT NULL /* 名前 */,
+"ruby" TEXT /* 名前のルビ */,
+"zip" TEXT /* 郵便番号 */,
+"address" TEXT /* 住所 */,
+"tel" TEXT /* 電話番号 */,
+"fax" TEXT /* FAX番号 */,
+"photo" BYTEA /* 写真 */,
 PRIMARY KEY ("id")
 );
+COMMENT ON TABLE "profile" IS 'プロフィール';
+COMMENT ON COLUMN "profile"."name" IS '名前';
+COMMENT ON COLUMN "profile"."ruby" IS '名前のルビ';
+COMMENT ON COLUMN "profile"."zip" IS '郵便番号';
+COMMENT ON COLUMN "profile"."address" IS '住所';
+COMMENT ON COLUMN "profile"."tel" IS '電話番号';
+COMMENT ON COLUMN "profile"."fax" IS 'FAX番号';
+COMMENT ON COLUMN "profile"."photo" IS '写真';
 
 CREATE TABLE "password" (
 "id"  SERIAL ,
@@ -25,9 +34,11 @@ PRIMARY KEY ("id")
 
 CREATE TABLE "note" (
 "id"  SERIAL ,
-"value" TEXT ,
+"value" TEXT /* 備考の内容 */,
 PRIMARY KEY ("id")
 );
+COMMENT ON TABLE "note" IS '備考';
+COMMENT ON COLUMN "note"."value" IS '備考の内容';
 
 CREATE TABLE "profile_note" (
 "id"  SERIAL ,
@@ -39,13 +50,19 @@ PRIMARY KEY ("id")
 
 CREATE TABLE "Company" (
 "id"  SERIAL ,
-"name" TEXT ,
-"brunch" TEXT ,
-"post" TEXT ,
-"tel" TEXT ,
-"fax" TEXT ,
+"name" TEXT /* 会社名 */,
+"brunch" TEXT /* 部署 */,
+"post" TEXT /* 役職 */,
+"tel" TEXT /* 電話番号 */,
+"fax" TEXT /* FAX番号 */,
 PRIMARY KEY ("id")
 );
+COMMENT ON TABLE "Company" IS '所属している会社の情報';
+COMMENT ON COLUMN "Company"."name" IS '会社名';
+COMMENT ON COLUMN "Company"."brunch" IS '部署';
+COMMENT ON COLUMN "Company"."post" IS '役職';
+COMMENT ON COLUMN "Company"."tel" IS '電話番号';
+COMMENT ON COLUMN "Company"."fax" IS 'FAX番号';
 
 CREATE TABLE "profile_company" (
 "id"  SERIAL ,
@@ -55,15 +72,28 @@ CREATE TABLE "profile_company" (
 PRIMARY KEY ("id")
 );
 
-CREATE TABLE "profile_profile" (
+CREATE TABLE "relational_profile" (
 "id"  SERIAL ,
-"id_profile" INTEGER NOT NULL ,
-"id_profile_other" INTEGER NOT NULL ,
-"timestamp" TIMESTAMP NOT NULL ,
-"location" GEOMETRY NOT NULL ,
-"photo" BYTEA NOT NULL ,
+"id_profile" INTEGER NOT NULL /* 自分のプロフィールID */,
+"id_profile_other" INTEGER NOT NULL /* 他者のプロフィールID */,
+"id_relational_status" INTEGER ,
 PRIMARY KEY ("id")
 );
+COMMENT ON TABLE "relational_profile" IS 'プロフィールの繋り';
+COMMENT ON COLUMN "relational_profile"."id_profile" IS '自分のプロフィールID';
+COMMENT ON COLUMN "relational_profile"."id_profile_other" IS '他者のプロフィールID';
+
+CREATE TABLE "relational_status" (
+"id"  SERIAL ,
+"timestamp" TIMESTAMP /* 出会いの時刻 */,
+"location" GEOMETRY /* 出会った場所 */,
+"photo" BYTEA /* 写真 */,
+PRIMARY KEY ("id")
+);
+COMMENT ON TABLE "relational_status" IS '繋りの状態';
+COMMENT ON COLUMN "relational_status"."timestamp" IS '出会いの時刻';
+COMMENT ON COLUMN "relational_status"."location" IS '出会った場所';
+COMMENT ON COLUMN "relational_status"."photo" IS '写真';
 
 ALTER TABLE "profile_password" ADD FOREIGN KEY ("id_profile") REFERENCES "profile" ("id");
 ALTER TABLE "profile_password" ADD FOREIGN KEY ("id_password") REFERENCES "password" ("id");
@@ -71,6 +101,7 @@ ALTER TABLE "profile_note" ADD FOREIGN KEY ("id_profile") REFERENCES "profile" (
 ALTER TABLE "profile_note" ADD FOREIGN KEY ("id_note") REFERENCES "note" ("id");
 ALTER TABLE "profile_company" ADD FOREIGN KEY ("id_profile") REFERENCES "profile" ("id");
 ALTER TABLE "profile_company" ADD FOREIGN KEY ("id_Company") REFERENCES "Company" ("id");
-ALTER TABLE "profile_profile" ADD FOREIGN KEY ("id_profile") REFERENCES "profile" ("id");
-ALTER TABLE "profile_profile" ADD FOREIGN KEY ("id_profile_other") REFERENCES "profile" ("id");
+ALTER TABLE "relational_profile" ADD FOREIGN KEY ("id_profile") REFERENCES "profile" ("id");
+ALTER TABLE "relational_profile" ADD FOREIGN KEY ("id_profile_other") REFERENCES "profile" ("id");
+ALTER TABLE "relational_profile" ADD FOREIGN KEY ("id_relational_status") REFERENCES "relational_status" ("id");
 
